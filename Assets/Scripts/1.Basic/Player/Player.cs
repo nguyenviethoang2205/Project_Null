d10926031a8858e4381cb43ae77c8d8aac96bc9d
nguyenviethoang2205 <96973433+nguyenviethoang2205@users.noreply.github.com>
2023-10-11 21:12:15 +0700

    select char

---
+++ Assets/Scripts/1.Basic/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterDB characterDB;
+    public GameObject player;
+    private int selectOption = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("selectOption")){
+            selectOption = 0;
+        }
+        else{
+            Load();
+        }
+        UpdateCharacter(selectOption);
+    }
+
+    private void UpdateCharacter(int selectOption){
+        Character character = characterDB.GetCharacter(selectOption);
+        player = character.player;
+    }
+
+    private void Load(){
+        selectOption = PlayerPrefs.GetInt("selectOption");
+    }
+}
