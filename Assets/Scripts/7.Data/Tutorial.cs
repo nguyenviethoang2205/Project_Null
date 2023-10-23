41e3b7f3e586480e9f3fdca7349fa884b31801e9
nguyenviethoang2205 <96973433+nguyenviethoang2205@users.noreply.github.com>
2023-10-23 14:09:10 +0700

    23/10 2:09

--- Assets/Scripts/7.Data/Tutorial.cs
+++ Assets/Scripts/7.Data/Tutorial.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 // [SerializeField] private Character character;
 // private IDataService DataService = new JsonDataService();
 // private bool EncryptionEnable;
-// private long saveTime;
 
+#region Save
+    // JsonConvert.SerializeObject(character, Formatting.Indented, 
+    //         new JsonSerializerSettings 
+    //         { 
+    //                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+    //         });
+    // if (DataService.SaveData("/characters.json", character, EncryptionEnable))
+    // {
+
+    //     Debug.Log(character);
+    // }
+    // else
+    // {
+    //     Debug.LogError("Could not save the file!");
+    // }
+#endregion
+
+
+#region Load
+    // Character charData = DataService.LoadData<Character>("/characters.json", EncryptionEnable);
+#endregion
 
