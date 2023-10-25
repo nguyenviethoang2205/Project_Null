376200f0b6609abf25147eedb24e1cc0461676f8
nguyenviethoang2205 <96973433+nguyenviethoang2205@users.noreply.github.com>
2023-10-26 02:12:44 +0700

    Revert "1:40"
    This reverts commit 222c206a4ad24521756d787ced7298c9d960df3d.

--- Assets/Scripts/4.Map/ZoneSelect.cs
+++ Assets/Scripts/4.Map/ZoneSelect.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using Cinemachine;
 using DG.Tweening;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Spine.Unity;
 using Spine;
 
-[JsonObject(MemberSerialization.OptIn)]
 public class ZoneSelect : MonoBehaviour
 {
     public bool isCompleted; //false
@@ -16,16 +14,7 @@ public class ZoneSelect : MonoBehaviour
     [SerializeField] private static GameObject player;
     [SerializeField] private GameObject startZone;
     [SerializeField] private GameObject selectionZone;
-    [JsonProperty] private float[] currentPos;
-
-    public float[] CurrentPos(GameObject _player)
-    {
-        currentPos = new float[3];
-        currentPos[0] = _player.transform.position.x;
-        currentPos[1] = _player.transform.position.y;
-        currentPos[2] = _player.transform.position.z;
-        return currentPos;
-    }
+    private Vector3 currentPos;
     public new Collider collider;
     public Path path;
 
@@ -56,32 +45,21 @@ public class ZoneSelect : MonoBehaviour
     }
 
     private void Start()
-    {
-
-    }
-
-    private void Update()
-    {
+    {   
+        Completed();
         UpdateZone();
     }
 
     public void UpdateZone()
     {
-        foreach (var i in path.zone)
+        if (!isCompleted)
         {
-
-            if (!isCompleted)
-            {
-                uncompleteOj.SetActive(true);
-            }
-            if (isCompleted)
-            {
-                i.SetActive(true);
-                uncompleteOj.SetActive(false);
-                collider.enabled = false;
-            }
+            uncompleteOj.SetActive(true);
         }
-
+        else
+        {
+            Completed();
+        }
     }
 
     private void OnMouseDown()
@@ -90,43 +68,24 @@ public class ZoneSelect : MonoBehaviour
         {
             path.isMove = true;
             Move();
-
+            
         }
     }
 
     public IEnumerator Completed()
     {
         yield return new WaitForSeconds(1);
-        gameObject.SetActive(true);
         uncompleteOj.SetActive(false);
+        isCompleted = false;
         collider.enabled = false;
     }
 
-    public void SavePos()
-    {
-        JsonConvert.SerializeObject(currentPos, Formatting.Indented,
-                new JsonSerializerSettings
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                });
-
-        if (DataService.SaveData("/position.json", currentPos, EncryptionEnable))
-        {
-
-            Debug.Log(currentPos);
-        }
-        else
-        {
-            Debug.LogError("Could not save the file!");
-        }
-    }
     public void Move()
     {
-        CurrentPos(player);
-        SavePos();
         player.transform.DOMove(selectionZone.transform.position, 1);
         StartCoroutine(Completed());
         StartCoroutine(ZoneSelected());
+
     }
 
     public IEnumerator ZoneSelected()
@@ -158,14 +117,15 @@ public class ZoneSelect : MonoBehaviour
 
                 case "Zone_4":
                     path.zone[7].SetActive(true);
+
                     SceneManager.LoadScene("Tetris", LoadSceneMode.Additive);
+
+                    // SceneManager.LoadScene("Tetris");
+                case "Zone_5":
+                    path.zone[7].SetActive(true);
+                    path.zone[6].SetActive(false);
                     // SceneManager.LoadScene("Tetris");
                     break;
-
-            case "Zone_4":
-                path.zone[7].SetActive(true);
-                // SceneManager.LoadScene("Tetris");
-                break;
 
                 case "Zone_6":
                     path.zone[8].SetActive(true);
