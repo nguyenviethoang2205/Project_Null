88c1a381e83b7051fb3a18725d1202e1be2c58d8
Dieter Walker <nguyenquocdue32@gmail.com>
2023-10-26 00:52:14 +0700

    update more items

---
+++ Assets/Scripts/1.Basic/Items/Items/OlekSkillBook.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "OlekSkillBook", menuName = "Game/OlekSkillBook")]
+public class OlekSkillBook : ItemBase
+{
+    public override void Initialize()
+    {
+        itemName = "Olek's martial arts secret";
+        itemInfo = "+10 to the current combo you have.";
+        itemImage = Resources.Load<Sprite>("Items/Item8");;
+    }
+
+    public override void UseItems(Boards boards){
+        boards.ItemsInsertCombo(10);
+    }
+}
