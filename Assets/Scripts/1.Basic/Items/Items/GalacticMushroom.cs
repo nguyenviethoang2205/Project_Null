d9ea16f39b52e099c3e195e2386eba87e4aeac55
Dieter Walker <nguyenquocdue32@gmail.com>
2023-10-28 00:19:45 +0700

    Item and SKillBar Update;

--- Assets/Scripts/1.Basic/Items/Items/GalacticMushroom.cs
+++ Assets/Scripts/1.Basic/Items/Items/GalacticMushroom.cs
@@ -5,13 +5,13 @@ public class GalacticMushroom : ItemBase
     public override void Initialize()
     {
         itemName = "Galactic Mushroom";
-        itemInfo = "You will become 'dizzy,' but the character's attacks deal an additional 5 damage.\n(There might be a way to counteract this effect)";
+        itemInfo = "You will become 'dizzy,' but the character's attacks deal an additional 10 damage.\n(There might be a way to counteract this effect)";
         itemImage = Resources.Load<Sprite>("Items/Item10");
     }
 
     public override void UseItems(Boards boards){
         boards.PlayerUseItemAnimation();
-        boards.ItemsInsertDamage(5);
+        boards.ItemsInsertDamage(10);
         boards.activePiece.status = "dizzy";
     }
 }
